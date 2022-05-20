e4709a4e1468e257dd9279cb483c3c5943bacab1
Burion <vladislavburyak00@gmail.com>
2022-05-20 16:07:43 +0300

    [+] IUsersAccesser, UsersCosmosAccesser

---
+++ ChattyDAL/Interfaces/IUsersAccesser.cs
@@ -0,0 +1,17 @@
+﻿using ChattyDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChattyDAL.Interfaces
+{
+    public interface IUsersAccesser
+    {
+        Task<User> UpsertUser(User user);
+        Task<User> GetUser(Expression<Func<User, bool>> predicate);
+        Task<IEnumerable<User>> GetUsers(Expression<Func<User, bool>> predicate);
+    }
+}
